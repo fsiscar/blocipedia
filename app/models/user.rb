4aaa83fdabb9559184d3abad89fcaafe464a93e4
fsiscar <fsiscar@gmail.com>
2018-05-21 08:19:26 -0500

    Blocipedia:User roles:Completed

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,11 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable, :confirmable
+
+  has_many :wikis
+
+  before_save { self.email = email.downcase }
+  before_save { self.role ||= :standard }
+
+  enum role: [:standard, :premium, :admin]
 end
